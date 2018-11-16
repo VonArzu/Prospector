247033a6112c1d8e80f190dbbd9fedc6a8a4286c
Von Arzu <vonarzu@gmail.com>
2018-11-15 19:20:17 -0500

    ScoreManager

--- Assets/__Scripts/ScoreManager.cs
+++ Assets/__Scripts/ScoreManager.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public enum eScoreEvent
+{
+    draw,
+    mine,
+    mineGold,
+    gameWin,
+    gameLoss
 }
+
+public class ScoreManager : MonoBehaviour
+{
+
+    static private ScoreManager S; // b
+
+    static public int SCORE_FROM_PREV_ROUND = 0;
+    static public int HIGH_SCORE = 0;
+
+    [Header("Set Dynamically")]
+    // Fields to track score info
+    public int chain = 0;
+    public int scoreRun = 0;
+    public int score = 0;
+
+    void Awake()
+    {
+        if (S == null)
+        { // c
+            S = this; // Set the private singleton
+        }
+        else
+        {
+            Debug.LogError("ERROR: ScoreManager.Awake(): S is already set!");
+        }
+        // Check for a high score in PlayerPrefs
+        if (PlayerPrefs.HasKey("ProspectorHighScore"))
+        {
+            HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
+        }
+        // Add the score from last round, which will be >0 if it was a win
+        score += SCORE_FROM_PREV_ROUND;
+        // And reset the SCORE_FROM_PREV_ROUND
+        SCORE_FROM_PREV_ROUND = 0;
+    }
