7ec7f4278c4dbbdcd2df4117bef410ea5ae56074
Von Arzu <vonarzu@gmail.com>
2018-11-15 19:04:33 -0500

    Settings & Card

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -4,52 +4,56 @@ using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
+    [Header("Set Dynamically")]
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  
+
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
+
+    public GameObject back;  
+    public CardDefinition def;  		
+
+    
+    public SpriteRenderer[] spriteRenderers;
+
+    void Start()
+    {
+        SetSortOrder(0); 
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    void Start () {
 	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
-
-
-	public bool faceUp {
-		get {
-			return (!back.activeSelf);
-		}
-
-		set {
-			back.SetActive(!value);
-		}
 	}
 
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 	
 	}
-} // class Card
+} 
 
 [System.Serializable]
 public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
+	public string	type;			
+	public Vector3	loc;			
+	public bool		flip = false;	
 	public float 	scale = 1.0f;
 }
 
 [System.Serializable]
 public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
+	public string	face;	
+	public int		rank;	
 	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+					pips = new List<Decorator>();  
 }
