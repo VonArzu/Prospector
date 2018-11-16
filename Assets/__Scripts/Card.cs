a03cdcd2c93a57340142b395f30be130e31f4084
Von Arzu <vonarzu@gmail.com>
2018-11-15 19:09:32 -0500

    Card Cont'd

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -33,27 +33,76 @@ public class Card : MonoBehaviour {
         }
     }
 
-    void Start () {
-	
-	}
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
 
-	void Update () {
-	
-	}
-} 
+    
+    public void SetSortOrder(int sOrd)
+    { 
+        PopulateSpriteRenderers();
+        
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                
+                tSR.sortingOrder = sOrd; 
+                continue; 
+            }
+          
+            switch (tSR.gameObject.name)
+            {
+                case "back": 
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face": 
+                default: 
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 
-[System.Serializable]
-public class Decorator{
-	public string	type;			
-	public Vector3	loc;			
-	public bool		flip = false;	
-	public float 	scale = 1.0f;
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name); 
+    }
+
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	
-	public int		rank;	
-	public List<Decorator>	
-					pips = new List<Decorator>();  
+public class Decorator
+{
+    public string type;         
+    public Vector3 loc;         
+    public bool flip = false;   
+    public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    public string face; 
+    public int rank;    
+    public List<Decorator>
+                    pips = new List<Decorator>();  
 }
